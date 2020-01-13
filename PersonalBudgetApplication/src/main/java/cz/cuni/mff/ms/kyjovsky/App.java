3b14c78296b51faa2eadae283a54fb8a01be8180
Michal Kyjovsky <michalkyjovsky@hotmail.com>
2020-01-13 09:41:15 +0100

    Initial commit, let'sstart programming

---
+++ PersonalBudgetApplication/src/main/java/cz/cuni/mff/ms/kyjovsky/App.java
@@ -0,0 +1,57 @@
+package cz.cuni.mff.ms.kyjovsky;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * JavaFX App
+ */
+public class App extends Application {
+
+    private static Scene scene;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+//        scene = new Scene(loadFXML("primary"), 640, 480);
+//        stage.setScene(scene);
+//        stage.show();
+//    }
+//
+//    static void setRoot(String fxml) throws IOException {
+//        scene.setRoot(loadFXML(fxml));
+        stage.setTitle("JavaFX App");
+
+        FileChooser fileChooser = new FileChooser();
+
+        Button button = new Button();
+        button.setOnAction(e -> {
+            File selectedFile = fileChooser.showOpenDialog(stage);
+        });
+
+        VBox vBox = new VBox(button);
+        Scene scene = new Scene(vBox, 960,600);
+
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
---
+++ PersonalBudgetApplication/src/main/java/cz/cuni/mff/ms/kyjovsky/PrimaryController.java
@@ -0,0 +1,12 @@
+package cz.cuni.mff.ms.kyjovsky;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class PrimaryController {
+
+    @FXML
+    private void switchToSecondary() throws IOException {
+//        App.setRoot("secondary");
+    }
+}
