ed4803b261fae01fbfe8c807171eb7c949a85336
Michal Kyjovsky <michalkyjovsky@hotmail.com>
2020-02-27 08:51:16 +0100

    landing page was upadted

--- PersonalBudgetApplication/src/main/java/cz/cuni/mff/ms/kyjovsm/ui/LandingPageController.java
+++ PersonalBudgetApplication/src/main/java/cz/cuni/mff/ms/kyjovsm/ui/LandingPageController.java
@@ -2,14 +2,27 @@ package cz.cuni.mff.ms.kyjovsm.ui;
 
 import cz.cuni.mff.ms.kyjovsm.additionalUtils.AlertBox;
 import cz.cuni.mff.ms.kyjovsm.workbook.WorkbookBuilder;
+
+import java.io.File;
 import java.io.IOException;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
 public class LandingPageController {
 
-    public Button createNewWorkbookButton;
-    public Button openFromLocalButton;
+    @FXML
+    private Button createNewWorkbookButton;
+    @FXML
+    private Button openFromLocalButton;
+    @FXML
+    private Button openFromCloudButton;
+    private static File chosenFile;
 
     /**
      * Method called when "Create New Workbook" button is pressed.
@@ -19,6 +32,7 @@ public class LandingPageController {
     public void createNewWorkbook(){
         WorkbookController wc = new WorkbookController();
         try {
+            disableButtonsOnClick(true);
             wc.createWorkbook();
         }catch (IOException e){
             e.printStackTrace();
@@ -26,8 +40,41 @@ public class LandingPageController {
     }
 
     public void displayFileExplorer(){
-        AlertBox alertBox = new AlertBox();
-        alertBox.displayAlertBox("AlertBoxEmptyInput");
+        Stage fileDialog = new Stage();
+        FileChooser fileChooser = new FileChooser();
+
+        try {
+            disableButtonsOnClick(true);
+            chosenFile = fileChooser.showOpenDialog(fileDialog);
+            if (chosenFile != null) {
+                App.changeScene(new Scene(loadFXMLforSheet("Sheet")));
+                System.out.println(chosenFile);
+            }else {
+                disableButtonsOnClick(false);
+            }
+        }catch (Exception e){
+            System.err.println("FXML load for opening from local has failed. Check it out!");
+        }
+
     }
 
+    private Parent loadFXMLforSheet(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(SheetBuilderController.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
+    }
+
+    public static File getChosenFile() {
+        return chosenFile;
+    }
+
+    private void disableButtonsOnClick(boolean status){
+        createNewWorkbookButton.setDisable(status);
+        openFromLocalButton.setDisable(status);
+        openFromCloudButton.setDisable(status);
+
+    }
+
+    public void openFromCloud(){
+        //TODO:Implement in feature releases, not in actual scope.
+    }
 }
