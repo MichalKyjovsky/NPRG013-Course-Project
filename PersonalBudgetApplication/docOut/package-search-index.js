264cb7b1ab0f84860326f096935334ca05e1559c
Michal Kyjovsky <michalkyjovsky@hotmail.com>
2020-03-15 14:05:57 +0100

    Cleanup

--- PersonalBudgetApplication/docOut/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"cz.cuni.mff.ms.kyjovsm.additionalUtils"},{"l":"cz.cuni.mff.ms.kyjovsm.ui"},{"l":"cz.cuni.mff.ms.kyjovsm.workbook"}];
